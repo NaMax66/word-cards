2bd88459869e8acf09852aeb7c4fcab4817e0c7f
NaMax66 <max.naid@mail.ru>
2022-11-21 17:28:15 +0600

    add sign in button

---
+++ src/components/GoogleAuth.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <div id="google-login-btn"></div>
+    <a href="#" @click.prevent="logout">sign out</a>
+  </div>
+</template>
+
+<script setup>
+function logout() {
+}
+
+function handleCredentialResponse(response) {
+  console.log('Encoded JWT ID token: ' + response.credential)
+}
+window.onload = function () {
+  google.accounts.id.initialize({
+    client_id: '',
+    callback: handleCredentialResponse
+  })
+  google.accounts.id.renderButton(
+      document.getElementById('google-login-btn'),
+      { theme: 'outline', size: 'large' }  // customization attributes
+  )
+  // google.accounts.id.prompt() // also display the One Tap dialog
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/views/AuthView.vue
+++ src/views/AuthView.vue
@@ -1,10 +1,18 @@
 <script setup lang="ts">
 </script>
 <template>
-  <div id="g_id_onload"
-       data-client_id="YOUR_GOOGLE_CLIENT_ID"
-       data-login_uri="https://your.domain/your_login_endpoint"
-       data-your_own_param_1_to_login="any_value"
-       data-your_own_param_2_to_login="any_value">
+  <div>
+    <div id="g_id_onload"
+         data-client_id=""
+         data-auto_prompt="false">
+    </div>
+    <div class="g_id_signin"
+         data-type="standard"
+         data-size="large"
+         data-theme="outline"
+         data-text="sign_in_with"
+         data-shape="rectangular"
+         data-logo_alignment="left">
+    </div>
   </div>
 </template>
