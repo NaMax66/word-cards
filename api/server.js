4f2c9578b5d0934cb304934addb6fa28ba0bcbce
NaMax66 <max.naid@mail.ru>
2022-11-23 16:50:45 +0600

    add user data when signed in

--- api/server.js
+++ api/server.js
@@ -1,13 +1,15 @@
 import express from 'express'
 import bodyParser from 'body-parser'
+import cookieParser from 'cookie-parser'
 import { OAuth2Client } from 'google-auth-library'
 
 const client = new OAuth2Client(process.env.APP_GOOGLE_CLIENT_ID)
 
 const app = express()
 
+app.use(cookieParser())
 app.use(bodyParser.urlencoded({ extended: true }))
-app.use((req, res, next) => {
+app.use(async (req, res, next) => {
   res.append('Access-Control-Allow-Origin', ['http://localhost:5173'])
   res.append('Access-Control-Allow-Methods', 'GET,POST')
   res.append('Access-Control-Allow-Headers', 'Content-Type')
@@ -18,16 +20,10 @@ app.post('/login', async (req, res) => {
   try {
     const { credential } = req.body
 
-    const ticket = await client.verifyIdToken({
-      idToken: credential,
-      audience: process.env.APP_GOOGLE_CLIENT_ID,
-    })
-    const payload = ticket.getPayload()
-    const userid = payload['sub']
-    console.log(userid)
+    const userData = await verifyUser(credential)
     res.append('Access-Control-Allow-Credentials', true)
     res.cookie('session-token', credential, { domain: 'localhost' })
-    res.send('success')
+    res.send({ status: 'success', userData })
   } catch (e) {
     console.error(e)
     res.send('error')
@@ -37,6 +33,14 @@ app.post('/login', async (req, res) => {
 app.get('/word-list', (req, res) => {
 
 })*/
+
+app.get('/user-data', async (req, res) => {
+  res.append('Access-Control-Allow-Credentials', true)
+  const credential = req.cookies['session-token']
+  const userData = await verifyUser(credential)
+
+  res.send({ status: 'success', userData })
+})
 
 app.get('/logout', (req, res) => {
   res.append('Access-Control-Allow-Credentials', true)
@@ -49,3 +53,16 @@ const port = process.env.APP_PORT
 app.listen(port, () => {
   console.log(`App listening on port ${port}`)
 })
+
+async function verifyUser(credential) {
+  const ticket = await client.verifyIdToken({
+    idToken: credential,
+    audience: process.env.APP_GOOGLE_CLIENT_ID,
+  })
+  const payload = ticket.getPayload()
+
+  return {
+    name: payload.name,
+    picture: payload.picture
+  }
+}
--- src/components/GoogleAuth.vue
+++ src/components/GoogleAuth.vue
@@ -1,10 +1,12 @@
 <template>
   <div>
     <div v-show="!isSignedIn" id="google-login-btn"></div>
-    <div v-show="isSignedIn">
-      <img :src="userInfo.img" alt="user photo">
-      <h4>{{ userInfo.name }}</h4>
-      <a href="#" @click.prevent="logout">sign out</a>
+    <div class="signed-out-controller" v-show="isSignedIn">
+      <img class="user-photo" :src="userInfo.picture" alt="user photo">
+      <div class="ml-2">
+        <h4>{{ userInfo.name }}</h4>
+        <a href="#" @click.prevent="logout">sign out</a>
+      </div>
     </div>
   </div>
 </template>
@@ -14,33 +16,42 @@ import axios from 'axios'
 import Cookies from 'js-cookie'
 import { ref } from 'vue'
 
-async function logout() {
-  await axios.get('http://localhost:8080/logout', {
-    withCredentials: true
-  })
-  isSignedIn.value = checkIsSignedIn()
-}
-
 const isSignedIn = ref(checkIsSignedIn())
-
-const userInfo = ref({
-  name: '',
-  img: ''
-})
-
-async function handleCredentialResponse({ credential }) {
-  await axios.post('http://localhost:8080/login', { credential }, {
-    headers: {
-      'Content-Type': 'application/x-www-form-urlencoded'
-    },
-    withCredentials: true
-  })
-
-  isSignedIn.value = checkIsSignedIn()
-}
 
 function checkIsSignedIn() {
   return !!Cookies.get('session-token')
+}
+
+const userInfo = ref({
+  name: '',
+  picture: '',
+
+  setUserInfo({ name, picture }) {
+    this.name = name
+    this.picture = picture
+  },
+  clearUserInfo() {
+    this.name = ''
+    this.picture = ''
+  },
+
+  async fetchData() {
+    const { data: { userData } } = await axios.get('http://localhost:8080/user-data', {
+      withCredentials: true
+    })
+
+    userInfo.value.setUserInfo(userData)
+  }
+})
+
+if (isSignedIn.value) {
+  userInfo.value.fetchData()
+}
+
+async function logout() {
+  await axios.get('http://localhost:8080/logout', { withCredentials: true })
+  isSignedIn.value = checkIsSignedIn()
+  userInfo.value.clearUserInfo()
 }
 
 window.onload = function () {
@@ -50,13 +61,34 @@ window.onload = function () {
   })
   google.accounts.id.renderButton(
       document.getElementById('google-login-btn'),
-      { theme: 'outline', size: 'large' }  // customization attributes
+      { theme: 'outline', size: 'large' }
   )
 
-  // google.accounts.id.prompt() // also display the One Tap dialog
+  // google.accounts.id.prompt()
 }
+async function handleCredentialResponse({ credential }) {
+  const { data: { userData } } = await axios.post('http://localhost:8080/login', { credential }, {
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded'
+    },
+    withCredentials: true
+  })
+  userInfo.value.setUserInfo(userData)
+
+  isSignedIn.value = checkIsSignedIn()
+}
+
 </script>
 
 <style scoped>
+.user-photo {
+  max-width: 60px;
+  max-height: 60px;
+  border-radius: 50%;
+}
 
+.signed-out-controller {
+  display: flex;
+  align-items: center;
+}
 </style>
