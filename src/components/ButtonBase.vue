a4b7fbf3b32d5a547b7cb3f23ecbb869ddf160d5
NaMax66 <max.naid@mail.ru>
2022-12-03 15:59:17 +0600

    add colors on the table

---
+++ src/components/ButtonBase.vue
@@ -0,0 +1,25 @@
+<script lang="ts" setup>
+</script>
+
+<template>
+  <button class="button-base">
+    <slot></slot>
+  </button>
+</template>
+
+<style scoped>
+.button-base {
+  color: var(--main-contrast);
+  background: none;
+  border: none;
+  border-radius: var(--default-b-radius);
+  box-shadow: var(--main-shodow-top);
+  outline: none;
+  transition: all .1s;
+}
+
+.button-base:active {
+  color: var(--main-contrast-hard);
+  box-shadow: var(--main-shodow-bottom)
+}
+</style>
