da6b7ff19df74d0486839ebc6bb98f5b57f1d07d
NaMax66 <max.naid@mail.ru>
2022-12-10 14:03:59 +0600

    add scroll

---
+++ src/services/scroller.ts
@@ -0,0 +1,12 @@
+export function scrollBottom(element: HTMLElement | null, isSmooth = false) {
+  console.log(element)
+  if(!element) return
+
+  if(isSmooth) {
+    element.animate({
+      scrollTop: element.scrollHeight
+    }, 500)
+  } else {
+    element.scrollTop = element.scrollHeight
+  }
+}
--- src/views/WordsList.vue
+++ src/views/WordsList.vue
@@ -1,10 +1,18 @@
 <script lang="ts" setup>
 import WordsTable from '@/components/WordsTable.vue'
 import AddPair from '@/components/AddPair.vue'
+import { onMounted, ref } from 'vue'
+import { scrollBottom } from '@/services/scroller'
+
+const wordListPage = ref(null)
+
+onMounted(() => {
+  // scrollBottom(wordListPage.value)
+})
 </script>
 
 <template>
-  <main class="word-list">
+  <main ref="wordListPage" class="word-list">
     <words-table class="word-table" />
     <add-pair class="pb-3" />
   </main>
@@ -14,8 +22,16 @@ import AddPair from '@/components/AddPair.vue'
 .word-list {
   position: relative;
   display: flex;
-  flex-direction: column;
+  flex-direction: column-reverse;
   justify-content: space-between;
+  overflow-y: auto;
+  height: calc(100vh - 80px);
+  scrollbar-width: none;
+  -ms-overflow-style: none;
+}
+
+.word-list::-webkit-scrollbar {
+  display: none;
 }
 
 .word-table {
